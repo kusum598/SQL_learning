707eff5ac078da80c7c391ab10744afafb039877
kusum598 <kusumtanwar598@gmail.com>
2024-05-25 18:12:28 +0530

    jhghjg

---
+++ first_program.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 18:10:32 2024
+
+@author: kusum
+"""
+
+import pygame
+import random
+import math
+from pygame import mixer
+
+# Initialize pygame
+pygame.init()
+
+width, height = 800, 600
+
+# Creating pygame window
+screen = pygame.display.set_mode((width, height))
+
+# background sound
+mixer.music.load('background.wav')
+mixer.music.play(-1)
+
+
+
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.py.png')
+pygame.display.set_icon(icon)
+# Title and Icon
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.py.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
